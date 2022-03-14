60cdf08ada542a7b816dde4c891a067358145a06
Kirill Kashelev <96053187+kkirik185@users.noreply.github.com>
2022-03-15 00:28:29 +0300

    доьавил ии врага

---
+++ play1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;//подключил определение маршрута 
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public float dist;
+    NavMeshAgent nav;
+    public float Radius = 15;
+    public bool OnEnemy = false;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>(); 
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //расчет дистанции от игрока до enemy
+        dist = Vector3.Distance(player.transform.position, transform.position);
+        if(OnEnemy == true)
+        {
+         if(dist > Radius)
+        {
+            nav.enabled = false;
+            anim.SetBool("idle", true);
+            anim.SetBool("run", false);
+               anim.SetBool("idleidle",false);
+        }
+        if(dist < Radius)
+        {
+            nav.enabled = true;
+            nav.SetDestination(player.transform.position);
+            anim.SetBool("run", true);
+             anim.SetBool("idle", false);
+             anim.SetBool("idleidle",false);
+        }
+        }
+         else if (OnEnemy == false)
+        {
+              nav.enabled = false;
+               anim.SetBool("idleidle",true);
+              anim.SetBool("run", false);
+             anim.SetBool("idle", false);
+            
+        }
+       
+       
+    }
+    
+}
---
+++ play1/Assets/Scripts/EnemyOn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOn : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player")
+        {
+            Instantiate(Enemy, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
